57c455ea561f6bc92de90a0bbe3373ccc5b0c6e4
YDziashkevich <y.dziashkevich@gmail.com>
2015-05-07 23:42:17 +0300

    with ncurses

--- task2/cal.php
+++ task2/cal.php
@@ -3,5 +3,13 @@
 echo `clear`;
 date_default_timezone_set('UTC');
 require_once 'autoloader.php';
+//ncurses
 $caledar = new Calendar();
-echo $caledar->getCalendar();
+$ncurse = ncurses_init();
+$fullscreen = ncurses_newwin(0, 0, 0, 0);
+$small = ncurses_newwin(14, 24, 5, 10);
+ncurses_refresh();
+ncurses_mvwaddstr($small, 0, 0, $caledar->getCalendar());
+ncurses_wrefresh($small);
+ncurses_end();
+//echo $caledar->getCalendar();
